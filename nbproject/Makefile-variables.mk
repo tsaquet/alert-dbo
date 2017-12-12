2af27b960de2a538501d66773b63033a8661a758
tsaquet <tsaquet@tsa-laptop>
2017-12-12 19:41:51 +0100

    netbeans project files updated to repair compil

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,29 +7,29 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug_SharedObject configuration
-CND_PLATFORM_Debug_SharedObject=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux-x86
+CND_PLATFORM_Debug_SharedObject=GNU-Linux
+CND_ARTIFACT_DIR_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux
 CND_ARTIFACT_NAME_Debug_SharedObject=libdbo.so
-CND_ARTIFACT_PATH_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux-x86/libdbo.so
-CND_PACKAGE_DIR_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux/libdbo.so
+CND_PACKAGE_DIR_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux/package
 CND_PACKAGE_NAME_Debug_SharedObject=libdbo.so.tar
-CND_PACKAGE_PATH_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux-x86/package/libdbo.so.tar
+CND_PACKAGE_PATH_Debug_SharedObject=dist/Debug_SharedObject/GNU-Linux/package/libdbo.so.tar
 # Release_SharedObject configuration
-CND_PLATFORM_Release_SharedObject=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release_SharedObject=dist/Release_SharedObject/GNU-Linux-x86
+CND_PLATFORM_Release_SharedObject=GNU-Linux
+CND_ARTIFACT_DIR_Release_SharedObject=dist/Release_SharedObject/GNU-Linux
 CND_ARTIFACT_NAME_Release_SharedObject=libdbo.so
-CND_ARTIFACT_PATH_Release_SharedObject=dist/Release_SharedObject/GNU-Linux-x86/libdbo.so
-CND_PACKAGE_DIR_Release_SharedObject=dist/Release_SharedObject/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Release_SharedObject=dist/Release_SharedObject/GNU-Linux/libdbo.so
+CND_PACKAGE_DIR_Release_SharedObject=dist/Release_SharedObject/GNU-Linux/package
 CND_PACKAGE_NAME_Release_SharedObject=libdbo.so.tar
-CND_PACKAGE_PATH_Release_SharedObject=dist/Release_SharedObject/GNU-Linux-x86/package/libdbo.so.tar
+CND_PACKAGE_PATH_Release_SharedObject=dist/Release_SharedObject/GNU-Linux/package/libdbo.so.tar
 # Create_Tables configuration
-CND_PLATFORM_Create_Tables=GNU-Linux-x86
-CND_ARTIFACT_DIR_Create_Tables=dist/Create_Tables/GNU-Linux-x86
+CND_PLATFORM_Create_Tables=GNU-Linux
+CND_ARTIFACT_DIR_Create_Tables=dist/Create_Tables/GNU-Linux
 CND_ARTIFACT_NAME_Create_Tables=dbo
-CND_ARTIFACT_PATH_Create_Tables=dist/Create_Tables/GNU-Linux-x86/dbo
-CND_PACKAGE_DIR_Create_Tables=dist/Create_Tables/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Create_Tables=dist/Create_Tables/GNU-Linux/dbo
+CND_PACKAGE_DIR_Create_Tables=dist/Create_Tables/GNU-Linux/package
 CND_PACKAGE_NAME_Create_Tables=dbo.tar
-CND_PACKAGE_PATH_Create_Tables=dist/Create_Tables/GNU-Linux-x86/package/dbo.tar
+CND_PACKAGE_PATH_Create_Tables=dist/Create_Tables/GNU-Linux/package/dbo.tar
 #
 # include compiler specific variables
 #
